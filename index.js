c554098efeb7b6e33d70593f9e7de00cb17a035e
D14na Support <support@d14na.org>
2018-08-15 04:20:17 -0400

    Staging for Native Navigation.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // @flow
 
-import App from './src/app'
+// import App from './src/app'
+import { AppRegistry } from 'react-native'
+import App from './App'
+import { name as appName } from './app.json'
+
+AppRegistry.registerComponent(appName, () => App)
