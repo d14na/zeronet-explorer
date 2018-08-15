5f5a0bec1db67968b6879bf3af0e5daecfbb6c48
D14na Support <support@d14na.org>
2018-08-15 04:57:08 -0400

    Added bugsnag.

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,14 @@
 import { Navigation } from 'react-native-navigation'
 import App from './App'
 
+/* Silence 'unneeded' yellow box alerts. */
+console.ignoredYellowBox = [
+    'Remote debugger',
+    'Debugger and device times',
+    'Warning: componentWillMount is deprecated', // FIXME RN v.0.54.0
+    'Warning: componentWillReceiveProps is deprecated', // FIXME RN v.0.54.0
+]
+
 Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App)
 
 Navigation.events().registerAppLaunchedListener(() => {
