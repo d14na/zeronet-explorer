0bf2aba62f09982d92d4eeb31b45bb21fa1fd259
D14na Support <support@d14na.org>
2019-08-28 07:41:53 -0400

    Restored image resources.

--- App.js
+++ App.js
@@ -40,7 +40,7 @@ const App = () => {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionTitle}>Zeronet Explorer</Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
