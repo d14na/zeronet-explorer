0631087e24e39f941d4ecf4538274bafb59b891f
D14na Support <support@d14na.org>
2019-08-29 01:56:54 -0400

    Even prettier.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
 };
